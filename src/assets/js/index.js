d318201afc4c921a859e2d58c90731025e7b2db7
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-10-30 20:08:51 +0700

    section 5 rull

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -31,3 +31,42 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+const swipper2= new Swiper(".swipper-2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 2,
+  centeredSlides: true,
+  speed: 300,
+  grabCursor: true,
+  parallax: true,
+  spaceBetween: 10,
+
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
