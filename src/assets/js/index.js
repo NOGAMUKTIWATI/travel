1fd254610da19e187611a1a9d4ceda95030d1802
sahrullah <mohammadsahrullah894@gmail.com>
2023-11-02 02:38:52 +0700

    fixing

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -17,7 +17,7 @@ const swiper = new Swiper(".swiper", {
   },
 
   breakpoints: {
-    640: {
+    340: {
       slidesPerView: 1,
       spaceBetween: 20,
     },
@@ -32,40 +32,39 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-const swiper2 = new Swiper(".swiper-2", {
+const swiperTesti = new Swiper(".swiper-testimonial", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 2,
-  centeredSlides: true,
-  speed: 300,
-  grabCursor: true,
-  parallax: true,
-  spaceBetween: 10,
+  slidesPerView: 1,
+  spaceBetween: 30,
 
-  // // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-testimonial-pagination",
+  },
 
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: ".swiper-next",
-  //   prevEl: ".swiper-prev",
-  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-testimonal-next",
+    prevEl: ".swiper-testimonal-prev",
+  },
+});
 
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  merge: true,
+  loop: true,
+  margin: 0,
+  video: true,
+  lazyLoad: true,
+  center: true,
+  responsive: {
+    480: {
+      items: 2,
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
+    600: {
+      items: 4,
     },
   },
 });
